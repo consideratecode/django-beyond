ba38d83ce0d761cb7cc9dd10cad635d4b971e433
Daniel Hepper <daniel.hepper@epicco.de>
2018-03-15 11:39:44 +0100

    Added profile page to URL definitions

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -15,9 +15,13 @@ Including another URLconf
 """
 from django.urls import include, path
 from django.contrib import admin
+from django.views.generic import TemplateView
 
 urlpatterns = [
     path('polls/', include('polls.urls')),
     path('admin/', admin.site.urls),
     path('accounts/', include('django.contrib.auth.urls')),
+    path('accounts/profile/',
+         TemplateView.as_view(template_name='accounts/profile.html'),
+         name='profile'),
 ]
